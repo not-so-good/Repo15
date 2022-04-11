12622882e3e6389f35543390d15b5798fd35e643
Sanjin Suljanovic <sanjin.suljanovic@gmail.com>
2022-04-11 22:22:27 +0200

    The rest parameter with function parameters

---
+++ ex08/src/index.js
@@ -0,0 +1,7 @@
+const product = (...args) => args.reduce((a, b) => a * b, 1);
+
+console.log(product(1, 2));
+console.log(product(4, 3, 10, 2));
+console.log(product(7));
+console.log(product());
+module.exports = product;
